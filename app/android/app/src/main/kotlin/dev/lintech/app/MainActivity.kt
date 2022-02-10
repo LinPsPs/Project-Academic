6b4721356addbaed2b9b9355b6c6b505b2e915ee
Lin <haolin.yu@outlook.com>
2022-02-10 01:08:45 -0500

    init

---
+++ app/android/app/src/main/kotlin/dev/lintech/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.lintech.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'PDF Viewer',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late PdfViewerController _pdfViewerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfViewerController = PdfViewerController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Syncfusion Flutter PdfViewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_up,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _pdfViewerController.previousPage();
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _pdfViewerController.nextPage();
+            },
+          )
+        ],
+      ),
+      body: SfPdfViewer.asset(
+        'assets/docs/spot.pdf',
+        // canShowScrollHead: true,
+        // canShowScrollStatus: true,
+        // controller: _pdfViewerController,
+      ),
+    );
+  }
+}
